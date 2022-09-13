56787c10a3c9d82f4e1b4942a9f35fbadb752ad6
Alyx Ferrari <rubiquikyt@gmail.com>
2022-09-13 13:18:33 -0400

    Initial Commit

---
+++ Sobretium/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sobretium
+//
+//  Created by Alyx Ferrari on 9/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sobretium/SobretiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SobretiumApp.swift
+//  Sobretium
+//
+//  Created by Alyx Ferrari on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SobretiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
