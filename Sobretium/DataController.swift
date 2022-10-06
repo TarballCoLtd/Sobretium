182a87019595f6efbd75121d7e4bd75d664f2f3b
Alyx Ferrari <alyx.ferrari@gmail.com>
2022-10-06 14:22:32 -0400

    Add Apple Watch target

--- Sobretium/DataController.swift
+++ Sobretium/DataController.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreData
 
 class DataController: ObservableObject {
-    let container = NSPersistentContainer(name: "SobrietyEntry")
+    let container = NSPersistentCloudKitContainer(name: "SobrietyEntry")
     init() {
         container.loadPersistentStores { description, error in
             if let error = error {
---
+++ SobretiumWatch Watch App/SobretiumWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SobretiumWatchApp.swift
+//  SobretiumWatch Watch App
+//
+//  Created by Tarball on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SobretiumWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
