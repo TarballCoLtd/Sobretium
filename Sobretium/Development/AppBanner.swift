b32b686fa9feccb65e670bec7050ab1c887b2f99
Alyx Ferrari <alyx.ferrari@gmail.com>
2022-10-29 13:34:59 -0400

    Add .DS_Store to `.gitignore`

--- Sobretium/Development/AppBanner.swift
+++ Sobretium/Development/AppBanner.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct AppBanner: View {
+    let hueColors = stride(from: 0.57, to: 0.8, by: 0.001).map {
+        Color(hue: $0, saturation: 1, brightness: 1)
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+                .frame(height: 50)
+            HStack {
+                VStack {
+                    Image("SobretiumIcon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .cornerRadius(50)
+                        .shadow(color: .black, radius: 20)
+                        .padding()
+                    Text("Sobretium")
+                        .font(.system(size: 70, weight: .bold))
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(
+                            LinearGradient(colors: [.purple, .red, .teal], startPoint: .leading, endPoint: .trailing)
+                            //LinearGradient(gradient: Gradient(colors: hueColors), startPoint: .leading, endPoint: .trailing)
+                        )
+                        .padding()
+                }
+                Image("iPhone")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .frame(maxWidth: 500)
+            }
+            Spacer()
+        }
     }
 }
 
