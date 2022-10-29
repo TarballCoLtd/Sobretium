f5a2cbcbe0cfa954ff55e1f3b28425a9c690baac
Alyx Ferrari <alyx.ferrari@gmail.com>
2022-10-29 13:28:46 -0400

    Remove .DS_Store

---
+++ Sobretium/Development/AppBanner.swift
@@ -0,0 +1,20 @@
+//
+//  AppBanner.swift
+//  Sobretium
+//
+//  Created by Tarball on 10/25/22.
+//
+
+import SwiftUI
+
+struct AppBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppBanner_Previews: PreviewProvider {
+    static var previews: some View {
+        AppBanner()
+    }
+}
